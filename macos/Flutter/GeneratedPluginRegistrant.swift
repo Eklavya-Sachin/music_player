f35d163b9199c3a0a8fbdb5ef59cb9887539c6f9
Sachin Kumar <ojaz07860@gmail.com>
2022-11-23 10:39:16 +0530

    Autogenerated code only.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,12 @@
 import FlutterMacOS
 import Foundation
 
-import audioplayer
+import audioplayers_darwin
 import path_provider_macos
 import wakelock_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  AudioplayerPlugin.register(with: registry.registrar(forPlugin: "AudioplayerPlugin"))
+  AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   WakelockMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockMacosPlugin"))
 }
